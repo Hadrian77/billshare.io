128af009658a63b5cafe707396fd27ebd1b7ced0
Adrian Harris <60455439+Hadrian77@users.noreply.github.com>
2021-12-18 15:37:45 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function Person(number) {
+  this.income = document.querySelector(".income").value;
+}
+
+let financials = {};
+
+console.log(document.querySelector("#billList").children);
