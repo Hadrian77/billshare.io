06b825ffa7e58a1852e40012f577421386aa1dad
Adrian Harris <60455439+Hadrian77@users.noreply.github.com>
2022-01-02 01:35:47 -0500

    Added basic functionality

--- index.js
+++ index.js
@@ -1,9 +1,77 @@
 "use strict";
-
-function Person(number) {
-  this.income = document.querySelector(".income").value;
+// Models
+class Person {
+  constructor(element, totalincome) {
+    (this.income = element.value),
+      (this.incomeShare = this.income / totalincome),
+      (this.burden = 0);
+  }
 }
 
-let financials = {};
+class Bill {
+  constructor(element) {
+    this.cost = element.value;
+  }
+}
 
-console.log(document.querySelector("#billList").children);
+//Get and Store Info
+let billElementList = document.querySelectorAll(".bill");
+let incomeElementList = document.querySelectorAll(".income");
+let burdenElementList = document.querySelectorAll(".burden");
+
+function createBills() {
+  let bills = [];
+
+  for (let billElement of billElementList) {
+    bills.push(new Bill(billElement));
+  }
+
+  return bills;
+}
+
+function createPeople() {
+  let people = [];
+  let total = 0;
+
+  for (let incomeElement of incomeElementList) {
+    total += Number(incomeElement.value);
+  }
+
+  for (let incomeElement of incomeElementList) {
+    people.push(new Person(incomeElement, total));
+  }
+
+  return people;
+}
+
+function displayFinancials(people) {
+  //let billElementList = document.querySelectorAll(".bill");
+  console.log(burdenElementList);
+
+  for (let x = 0; x < burdenElementList.length; x++) {
+    console.log(people[x].burden);
+    burdenElementList[x].textContent = Math.round(people[x].burden * 100) / 100;
+  }
+}
+
+function calculate(bills, people) {
+  for (let bill of bills) {
+    for (let person of people) {
+      person.burden += bill.cost * person.incomeShare;
+    }
+  }
+}
+
+let people = createPeople();
+let bills = createBills();
+
+calculate(bills, people);
+
+let mainElement = document.querySelector("main");
+
+mainElement.addEventListener("change", function () {
+  const people = createPeople();
+
+  calculate(createBills(), people);
+  displayFinancials(people);
+});
